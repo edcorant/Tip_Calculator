246d0602f5fcfd90e1b4ac3db79e7f0fcf429508
Eduardo Antonini <eduardoantonini@icloud.com>
2020-07-03 12:23:24 -0400

    Initial Commit

---
+++ Tip_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip_Calculator
+//
+//  Created by Eduardo Antonini on 7/3/20.
+//  Copyright © 2020 Eduardo Antonini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
